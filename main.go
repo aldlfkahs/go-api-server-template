278e4801624652fc9d4838afd4764e3d291d51ca
Seungwon Lee <seungwon_lee@tmax.co.kr>
2022-01-18 15:31:30 +0900

    [mod] simple화 및 오타 수정

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"cloud-credential-api-server/billing"
 	"fmt"
-
 	"k8s.io/klog"
-
+	"encoding/json"
 	"net/http"
 )
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/billing", serveBilling)
 	mux.HandleFunc("/test", serveTest)
 	mux.HandleFunc("/hello", serveHello)
 
@@ -20,20 +17,9 @@ func main() {
 	klog.Flush()
 
 	if err := http.ListenAndServe(":80", mux); err != nil {
-		klog.Errorf("Failed to listen and serve AWS COST server: %s", err)
+		klog.Errorf("Failed to listen and serve test-api-server: %s", err)
 	}
-	klog.Info("Terminate Cloud Credential server")
-}
-
-func serveBilling(res http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case http.MethodGet:
-		billing.Get(res, req)
-	case http.MethodPut:
-	case http.MethodOptions:
-	default:
-		//error
-	}
+	klog.Info("Terminate test-api-server")
 }
 
 
@@ -41,8 +27,10 @@ func serveHello(res http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodGet:
 		fmt.Println("You call GET /hello !!")
+		SetResponse(res, "You call GET /hello !!", nil, http.StatusOK)
 	case http.MethodPost:
 		fmt.Println("You call POST /hello !!")
+		SetResponse(res, "You call POST /hello !!", nil, http.StatusOK)
 	case http.MethodOptions:
 	default:
 		//error
@@ -69,4 +57,33 @@ func TestGet(res http.ResponseWriter, req *http.Request) {
 		fmt.Println(k)
 	}
 	fmt.Println()
+}
+
+func SetResponse(res http.ResponseWriter, outString string, outJson interface{}, status int) http.ResponseWriter {
+
+	//set Cors
+	// res.Header().Set("Access-Control-Allow-Origin", "*")
+	res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	res.Header().Set("Access-Control-Max-Age", "3628800")
+	res.Header().Set("Access-Control-Expose-Headers", "Content-Type, X-Requested-With, Accept, Authorization, Referer, User-Agent")
+
+	//set Out
+	if outJson != nil {
+		res.Header().Set("Content-Type", "application/json")
+		js, err := json.Marshal(outJson)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+		}
+		//set StatusCode
+		res.WriteHeader(status)
+		res.Write(js)
+		return res
+
+	} else {
+		//set StatusCode
+		res.WriteHeader(status)
+		res.Write([]byte(outString))
+		return res
+
+	}
 }
